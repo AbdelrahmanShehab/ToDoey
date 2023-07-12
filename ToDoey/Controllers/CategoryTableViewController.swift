3a6ed72552507a9d9cb1efe6925473c6a1664998
AbdelrahmanShehab <abdoshehab7@gmail.com>
2023-07-12 04:23:53 +0300

    Adding Appicon

--- ToDoey/Controllers/CategoryTableViewController.swift
+++ ToDoey/Controllers/CategoryTableViewController.swift
@@ -51,13 +51,14 @@ class CategoryTableViewController: SwipeTableViewController {
         return cell
     }
 
-    // MARK: -  Table View Delegate
+    // MARK: -  Table View Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "GoToItems", sender: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destinationVC = segue.destination as! TodoListViewController  
+        let destinationVC = segue.destination as! TodoListViewController
         if let indexPath = tableView.indexPathForSelectedRow {
             destinationVC.selectedCategory = categories?[indexPath.row]
         }
